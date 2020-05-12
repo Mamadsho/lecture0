a38ae5184ae5ec6602880bc20bfefbbe3d4d4c0b
Mamadsho Nazaramonov <mamadsho.nazaramonov@gmail.com>
2020-05-12 13:35:15 +0500

    Testing touch interface and element dragging

---
+++ Project0/project0/DesignCourse/drag.js
@@ -0,0 +1,49 @@
+function startup(){
+var item=document.getElementById("item");
+var container=document.getElementById("container");
+
+var active=false;
+var curX;
+var curY;
+var initX;
+var initY;
+var xOffset=0;
+var yOffset=0;
+
+container.addEventListener("touchstart",dragStart, false);
+container.addEventListener("touchend",dragEnd, false);
+container.addEventListener("touchmove",drag,false);
+
+function dragStart(e){
+    initX=e.touches[0].clientX-xOffset;
+    initY=e.touches[0].clientY-yOffset;
+    if(e.target===item){
+        active=true;
+    }
+}
+function drag(e){
+    if(active){
+        e.preventDefault();
+
+        curX=e.touches[0].clientX-initX;
+        curY=e.touches[0].clientX-initY;
+
+        // xOffset=curX;
+        // yOffset=curY;
+
+        setTranslate(curX,CurY,item);
+    }
+}
+function dragEnd(e){
+    initX=curX;
+    initY=curY;
+
+    active=false;
+}
+function setTranslate(cX, cY,dragItem){
+    dragItem.style.transform="translate3d("+cX+"px, "+cY+"px, 0)";
+}
+}
+
+
+document.addEventListener("DOMContentLoaded",startup);
